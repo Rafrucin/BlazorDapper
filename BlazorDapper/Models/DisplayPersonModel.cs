0d2a1a0bc5e77b5431b45bf8e313a64b5cf226c0
Rafał Ruciński <rafal_rucinski@poczta.fm>
2020-06-14 19:26:50 +0100

    Add project files.

---
+++ BlazorDapper/Models/DisplayPersonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorDapper.Models
+{
+    public class DisplayPersonModel
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "First name too long")]
+        [MinLength (2, ErrorMessage ="First name too short")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(15, ErrorMessage = "Last name too long")]
+        [MinLength(2, ErrorMessage = "Last name too short")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ DataAccessLib/PeopleData.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLib
+{
+    public class PeopleData : IPeopleData
+    {
+        private readonly ISQLAccess _db;
+
+        public PeopleData(ISQLAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<List<PersonModel>> GetPeople()
+        {
+            string sql = "select * from dbo.people";
+            return _db.LoadData<PersonModel, dynamic>(sql, new { });
+        }
+
+        public Task InsertPerson(PersonModel person)
+        {
+            string sql = @"insert into dbo.People (FirstName, LastName, EmailAddress) 
+                            values (@FirstName, @LastName, @EmailAddress);";
+            return _db.SaveData(sql, person);
+        }
+    }
+}
